732738752475d36b1876d6c50c6b03147b410ca6
Baconator507 <ibarria0@gmail.com>
2013-11-03 22:22:13 -0500

    filters

--- app/controllers/licitations_controller.rb
+++ app/controllers/licitations_controller.rb
@@ -1,12 +1,12 @@
 class LicitationsController < ApplicationController
-  before_action :set_licitation, only: [:show, :edit, :update, :destroy]
+  before_action :set_licitation, only: [:edit, :update, :destroy]
 
   # GET /licitations
   # GET /licitations.json
   def index
     @licitations = Licitation.text_search(params[:query]).order('FECHA DESC')
     @licitations = @licitations.paginate(:page => params[:page])
-
+    filter_licitations
     @entidades = Rails.cache.fetch("entidades", :expires_in => 1.day ) {Licitation.select("DISTINCT(ENTIDAD)").map{|x| x.entidad}.sort}
     @compra_type = Rails.cache.fetch("compra_type", :expires_in => 1.day ) {Licitation.select("DISTINCT(COMPRA_TYPE)").select{|x| x.compra_type != 'Licitaciуn Pъblica'}.map{|x| x.compra_type }.sort}
     @categories = Category.all
@@ -15,6 +15,7 @@ class LicitationsController < ApplicationController
   # GET /licitations/1
   # GET /licitations/1.json
   def show
+    @licitation = Licitation.find_by_acto(params[:id])
   end
 
   # GET /licitations/new
@@ -76,4 +77,65 @@ class LicitationsController < ApplicationController
     def licitation_params
       params.require(:licitation).permit(:url, :visited, :parsed, :category_id, :compra_type, :entidad, :dependencia, :nombre_contacto, :telefono_contacto, :correo_contacto, :objeto, :modalidad, :unidad, :provincia, :precio, :proponente, :description, :acto, :fecha)
     end
+
+
+  def filter_fecha
+    if params[:fecha_min] and params[:fecha_max] and (params[:fecha_max] != '' or params[:fecha_min] != '') then
+      @licitations = @licitations.where('DATE(fecha) >= ?', params[:fecha_min]) if params[:fecha_min] != ''
+      @licitations = @licitations.where('DATE(fecha) <= ?', params[:fecha_max]) if params[:fecha_max] != ''
+    end
+  end
+
+  def filter_price
+    if params[:price_min] and params[:price_max] and (params[:price_max] != '' or params[:price_min] != '') then
+      @licitations = @licitations.where('precio >= ?', params[:price_min]) if params[:price_min] != ''
+      @licitations = @licitations.where('precio <= ?', params[:price_max]) if params[:price_max] != ''
+    end
+  end
+
+  def filter_proponente
+    if params[:proponente] and params[:proponente] != '' then
+      @licitations = @licitations.where("to_tsvector('simple',proponente) @@ plainto_tsquery('simple',?)", params[:proponente])
+    end
+  end
+
+  def filter_acto
+    if params[:acto] and params[:acto] != '' then
+      @licitations = @licitations.where("acto = ?", params[:acto])
+    end
+  end
+
+  def filter_modalidad
+    if params[:modalidad] and params[:modalidad] != '' then
+      @licitations = @licitations.where("modalidad = ?", params[:modalidad])
+    end
+  end
+
+  def filter_objeto
+    if params[:objeto] and params[:objeto] != '' then
+      @licitations = @licitations.where("objeto = ?", params[:objeto])
+    end
+  end
+
+  def filter_compra_type
+    if params[:compra_type] and params[:compra_type] != '' then
+      @licitations = @licitations.where("compra_type = ?", params[:compra_type])
+    end
+  end
+
+  def filter_licitations
+    filter_fecha
+    filter_price
+    filter_proponente
+    filter_acto
+    filter_modalidad
+    filter_objeto
+    filter_compra_type
+    @licitations = @licitations.where('entidad = ?', params[:entidad]) if (params[:entidad] and params[:entidad] != '')
+    @licitations = @licitations.where('category_id = ?', params[:category]) if (params[:category] and params[:category] != '')
+    @licitations = @licitations.where('proponente = ?', 'empty') if (params[:empty] and params[:empty] != '')
+  end
+
+
+
 end

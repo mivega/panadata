3cfb03689bb5a9e216182009420b4ae1f4d66d45
Baconator507 <ibarria0@gmail.com>
2013-11-04 13:07:17 -0500

    status helper

--- app/helpers/corporations_helper.rb
+++ app/helpers/corporations_helper.rb
@@ -1,2 +1,21 @@
 module CorporationsHelper
+
+    def status_label(corporation)
+        status = corporation.status
+        if status == 'VIGENTE'
+            render(inline: '<span class="label label-success">Vigente</span>')
+        elsif status == 'DISUELTA'
+            render(inline: '<span class="label label-danger">Disuelta</span>')
+        elsif status == 'REDOMICILIO PROVISIONAL'
+            render(inline: '<span class="label label-info">Redomicilio Provisional</span>')
+        elsif status == 'REDOMICILIO DEFINITIVO'
+            render(inline: '<span class="label label-primary">Redomicilio Definitivo</span>')
+        elsif status == 'FUSION'
+            render(inline: '<span class="label label-warning">Fusion</span>')
+        elsif status == 'TRANSFORMACION'
+            render(inline: '<span class="label label-default">Transformacion</span>')
+        end
+    end
+
+
 end

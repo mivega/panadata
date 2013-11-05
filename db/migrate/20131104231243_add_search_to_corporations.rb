2a79005badd5e2e0452fd3f0860109b419d39ad1
Baconator507 <ibarria0@gmail.com>
2013-11-04 21:44:22 -0500

    fixed function in migration

--- db/migrate/20131104231243_add_search_to_corporations.rb
+++ db/migrate/20131104231243_add_search_to_corporations.rb
@@ -6,7 +6,7 @@ class AddSearchToCorporations < ActiveRecord::Migration
 
     execute "CREATE FUNCTION sociedades_trigger() RETURNS trigger AS $$
             begin
-              new.tsv :=
+              new.tsv_nombre :=
                 setweight(to_tsvector('pg_catalog.spanish', coalesce(new.nombre, '')),'A') ||
                 setweight(to_tsvector(coalesce(CAST(new.ficha as text), '0')), 'B') || 
                 setweight(to_tsvector('pg_catalog.spanish', coalesce(new.agente,'')),'C') || 

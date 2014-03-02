bbdc405f30f585bc77a344f485c2e6a774981bd2
ibarria0@gmail.com <ibarria0@gmail.com>
2014-03-02 17:20:28 -0500

    removed stats for testing

--- app/controllers/licitations_controller.rb
+++ app/controllers/licitations_controller.rb
@@ -5,7 +5,7 @@ class LicitationsController < ApplicationController
     @total = @licitations.count
     @proponentes = @licitations.reject{ |l| l.proponente.nil? }.collect { |l| l.proponente }.group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
     @entidades = @licitations.collect { |l| l.entidad }.group_by{|x| x}.sort_by{|k, v| -v.size}.map(&:first)
-    @stats = DescriptiveStatistics::Stats.new(@licitations.collect { |l| l.precio })
+#    @stats = DescriptiveStatistics::Stats.new(@licitations.collect { |l| l.precio })
   end
 
   # GET /licitations

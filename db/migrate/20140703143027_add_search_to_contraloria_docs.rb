4cef3ae28eba8da5d1acb91afbd6f8f892919158
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-11 11:36:56 -0500

    contraloria numero de documento

--- db/migrate/20140703143027_add_search_to_contraloria_docs.rb
+++ db/migrate/20140703143027_add_search_to_contraloria_docs.rb
@@ -2,12 +2,13 @@ class AddSearchToContraloriaDocs < ActiveRecord::Migration
 
   def up
     add_column :documentos, :tsv_nombre, :tsvector
-    execute "UPDATE documentos SET tsv_nombre = (setweight(to_tsvector('pg_catalog.spanish', coalesce(institucion, '')),'A') || setweight(to_tsvector(coalesce(CAST(control as text), '0')), 'B') || setweight(to_tsvector('pg_catalog.spanish', coalesce(favor,'')),'C') || setweight(to_tsvector('pg_catalog.spanish', coalesce(documento,'')),'D'))"
+    execute "UPDATE documentos SET tsv_nombre = (setweight(to_tsvector('pg_catalog.spanish', coalesce(institucion, '')),'A') || setweight(to_tsvector(coalesce(CAST(numero as text), '0')), 'B') || setweight(to_tsvector(coalesce(CAST(control as text), '0')), 'B') || setweight(to_tsvector('pg_catalog.spanish', coalesce(favor,'')),'C') || setweight(to_tsvector('pg_catalog.spanish', coalesce(documento,'')),'D'))"
 
     execute "CREATE FUNCTION documentos_trigger() RETURNS trigger AS $$
             begin
               new.tsv_nombre :=
                 setweight(to_tsvector('pg_catalog.spanish', coalesce(new.institucion, '')),'A') ||
+                setweight(to_tsvector(coalesce(CAST(new.numero as text), '0')), 'B') || 
                 setweight(to_tsvector(coalesce(CAST(new.control as text), '0')), 'B') || 
                 setweight(to_tsvector('pg_catalog.spanish', coalesce(new.favor,'')),'C') ||
                 setweight(to_tsvector('pg_catalog.spanish', coalesce(new.documento,'')),'D');

e18956fe48cc7a68ab030888fb6f5a0925a6014e
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-15 01:07:44 -0500

    unicorn fix attempt 1

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -19,7 +19,7 @@ after_fork do |server, worker|
   end
 
   defined?(ActiveRecord::Base) and
-    config = Rails.application.config.database_configuration[Rails.env]
+    config = Rails.application.config.database_configuration[Rails.env] || ActiveRecord::Base.configurations[Rails.env]
     config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
     config['pool']              = ENV['DB_POOL'] || 5
     ActiveRecord::Base.establish_connection(config)

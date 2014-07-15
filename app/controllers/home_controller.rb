d0e85c982fa458f47ec215acb25585733aaae315
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-15 00:51:20 -0500

    cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @compras = Licitation.count
-    @sociedades = Corporation.count
-    @marcas = Brand.count
-    @docs = ContraloriaDoc.count
+
+    @compras = Rails.cache.fetch("compras_count", :expires_in => 1.day ) { Licitation.count }
+    @sociedades = Rails.cache.fetch("corporations_count", :expires_in => 1.day ) { Corporation.count }
+    @marcas = Rails.cache.fetch("brands", :expires_in => 1.day ) { Brand.count }
+    @docs = Rails.cache.fetch("contraloria_docs_count", :expires_in => 1.day ) { ContraloriaDoc.count }
   end
   def about
   end

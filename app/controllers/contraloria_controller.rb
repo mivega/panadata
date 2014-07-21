1729dc8dfd030965380681392bc560f4f08c5f23
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-21 10:27:58 -0500

    contraloria resumen

--- app/controllers/contraloria_controller.rb
+++ app/controllers/contraloria_controller.rb
@@ -1,6 +1,13 @@
 class ContraloriaController < ApplicationController
+
+  def stats
+    @total = @docs.length
+    @sum = @docs.sum(:monto)
+  end
+
   def index
     @docs = ContraloriaDoc.text_search(params[:query])
+    stats if params[:query]
     @docs = @docs.paginate(:page => params[:page])
   end
 

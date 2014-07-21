d9110acaff771ad4cf6a0f11e70379496ffd8d14
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-21 01:20:49 -0500

    about and faq

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,11 @@
 class HomeController < ApplicationController
   def index
-
-    @compras = Rails.cache.fetch("compras_count", :expires_in => 1.day ) { Licitation.count }
-    @sociedades = Rails.cache.fetch("corporations_count", :expires_in => 1.day ) { Corporation.count }
-    @marcas = Rails.cache.fetch("brands", :expires_in => 1.day ) { Brand.count }
-    @docs = Rails.cache.fetch("contraloria_docs_count", :expires_in => 1.day ) { ContraloriaDoc.count }
   end
   def about
+    @compras = Rails.cache.fetch("compras_count", :expires_in => 1.hour ) { Licitation.count }
+    @sociedades = Rails.cache.fetch("corporations_count", :expires_in => 1.hour ) { Corporation.count }
+    @marcas = Rails.cache.fetch("brands", :expires_in => 1.hour ) { Brand.count }
+    @docs = Rails.cache.fetch("contraloria_docs_count", :expires_in => 1.hour ) { ContraloriaDoc.count }
   end
   def stats 
   end

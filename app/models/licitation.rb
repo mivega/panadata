276d7ffa479425b01f8c036d405e186a3ec918f8
ibarria0@gmail.com <ibarria0@gmail.com>
2014-07-31 18:08:21 -0500

    include acto

--- app/models/licitation.rb
+++ app/models/licitation.rb
@@ -15,9 +15,9 @@ class Licitation < ActiveRecord::Base
     def self.text_search(query)
         if query.present?
 #          search(query)
-	    select('description,entidad,proponente,proveedor_id,precio,fecha').where("tsv_description @@ plainto_tsquery('pg_catalog.spanish',:q)" , q: query)
+	    select('acto,description,entidad,proponente,proveedor_id,precio,fecha').where("tsv_description @@ plainto_tsquery('pg_catalog.spanish',:q)" , q: query)
         else
-            Licitation.select('description,entidad,proponente,proveedor_id,precio,fecha')
+            Licitation.select('acto,description,entidad,proponente,proveedor_id,precio,fecha')
         end
     end
 

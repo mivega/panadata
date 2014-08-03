2648253c69c86145aae3f0db97c9c9a9188c3ad7
bacon <ibarria0@gmail.com>
2014-08-03 12:33:55 -0500

    top proveedores

--- app/controllers/proveedores_controller.rb
+++ app/controllers/proveedores_controller.rb
@@ -1,7 +1,11 @@
 class ProveedoresController < ApplicationController
 
   def index
-    @proveedores = Provider.text_search(params[:query]).paginate(:page => params[:page])
+    if params[:query] then
+      @proveedores = Provider.text_search(params[:query]).paginate(:page => params[:page])
+    else
+      @proveedores = Provider.select('proveedores.id,proveedores.nombre,count(*),sum(compras.precio)').joins(:licitations).group('proveedores.id,proveedores.nombre').where('compras.fecha > ?', 1.month.ago).order('count DESC').limit(10)
+    end
   end
 
   def show

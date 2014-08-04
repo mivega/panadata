e7b0e84994832c47e3cd518033ef24404efbb3e3
bacon <ibarria0@gmail.com>
2014-08-04 13:49:36 -0500

    brands fast show

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,71 +1,16 @@
 class BrandsController < ApplicationController
-  before_action :set_brand, only: [:show, :edit, :update, :destroy]
 
   # GET /brands
   # GET /brands.json
   def index
-    @brands = Brand.text_search(params[:query])
+    @brands = Brand.select('id,nombre,registro,abogado,fecha_registro').text_search(params[:query])
     @brands = @brands.paginate(:page => params[:page])
   end
 
   # GET /brands/1
   # GET /brands/1.json
   def show
+    @brand = Brand.select('id,nombre,registro,abogado,fecha_registro,estado,actividad,fecha_vencimiento,capital_text,representante_text').includes(:products,:owners).find(params[:id])
   end
-
-  # GET /brands/new
-  def new
-    @brand = Brand.new
-  end
-
-  # GET /brands/1/edit
-  def edit
-  end
-
-  # POST /brands
-  # POST /brands.json
-  def create
-    @brand = Brand.new(brand_params)
-
-    respond_to do |format|
-      if @brand.save
-        format.html { redirect_to @brand, notice: 'Brand was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @brand }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @brand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /brands/1
-  # PATCH/PUT /brands/1.json
-  def update
-    respond_to do |format|
-      if @brand.update(brand_params)
-        format.html { redirect_to @brand, notice: 'Brand was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @brand.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /brands/1
-  # DELETE /brands/1.json
-  def destroy
-    @brand.destroy
-    respond_to do |format|
-      format.html { redirect_to brands_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_brand
-      @brand = Brand.find(params[:id])
-    end
 
 end

e26c4f292e16740e21d17139db48ab74692175b9
bacon <ibarria0@gmail.com>
2014-08-25 11:42:46 -0500

    remove counts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,10 @@ class HomeController < ApplicationController
 
   def index
     #@compras_chart = licitation_chart_data(Licitation.where('fecha > ?', 3.month.ago))
-    @compras = Rails.cache.fetch("compras_count", :expires_in => 10.minutes) { Licitation.count }
-    @sociedades = Rails.cache.fetch("corporations_count", :expires_in => 1.hour ) { Corporation.count }
-    @marcas = Rails.cache.fetch("brands", :expires_in => 1.hour ) { Brand.count }
-    @docs = Rails.cache.fetch("contraloria_docs_count", :expires_in => 1.hour ) { ContraloriaDoc.count }
+#    @compras = Rails.cache.fetch("compras_count", :expires_in => 10.minutes) { Licitation.count }
+#    @sociedades = Rails.cache.fetch("corporations_count", :expires_in => 1.hour ) { Corporation.count }
+#    @marcas = Rails.cache.fetch("brands", :expires_in => 1.hour ) { Brand.count }
+#    @docs = Rails.cache.fetch("contraloria_docs_count", :expires_in => 1.hour ) { ContraloriaDoc.count }
   end
 
   def about

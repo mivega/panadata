5d7b00237d522ca43a0335c046709303aec39f2e
bacon <ibarria0@gmail.com>
2014-08-29 10:37:53 -0500

    caching in entidad

--- app/controllers/entidades_controller.rb
+++ app/controllers/entidades_controller.rb
@@ -5,9 +5,9 @@ class EntidadesController < ApplicationController
 
   def show
       @entidad = Entidad.find(params[:id])
-      @top_proveedores = Provider.select('proveedores.id,proveedores.nombre,count(*),sum(compras.precio)').joins(:licitations).group('proveedores.id,proveedores.nombre').where('compras.fecha > ? and compras.entidad_id = ?', "2014-07-01", @entidad.id).order('sum DESC').limit(10)
+      @top_proveedores = Rails.cache.fetch("/entidades/#{@entidad.id}/top_proveedores", :expires_in => 1.hour ) {Provider.select('proveedores.id,proveedores.nombre,count(*),sum(compras.precio)').joins(:licitations).group('proveedores.id,proveedores.nombre').where('compras.fecha > ? and compras.entidad_id = ?', "2014-07-01", @entidad.id).order('sum DESC').limit(10) }
       @licitations = Licitation.text_search('').where('fecha is not null').where(entidad_id: @entidad.id)
-      @chart_data = licitation_chart_data
+      @chart_data = Rails.cache.fetch("/entidades/#{@entidad.id}/chart", :expires_in => 1.hour ) {licitation_chart_data}
       @licitations = @licitations.order('FECHA DESC').paginate(:page => params[:page],total_entries:  @entidad.licitations.size)
   end
 end

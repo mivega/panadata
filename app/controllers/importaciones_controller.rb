f64cde85e873f70517e687e1c9ef3ae71a3ac983
bacon <ibarria0@gmail.com>
2014-09-17 16:18:02 -0500

    order by fecha desc

--- app/controllers/importaciones_controller.rb
+++ app/controllers/importaciones_controller.rb
@@ -3,7 +3,7 @@ class ImportacionesController < ApplicationController
 
 
   def index
-    @importaciones = Importacion.text_search(params[:query]).paginate(:page => params[:page])
+    @importaciones = Importacion.text_search(params[:query]).order('FECHA DESC').paginate(:page => params[:page])
   end
 
 end
